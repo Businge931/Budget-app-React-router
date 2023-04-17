13e3e8f71639df4d2c3eaf89969a0f3f5434f674
Businge931 <jimmybusinge9@gmail.com>
2023-04-18 00:34:16 +0300

    feat: set up project routes, actions and loaders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+//rrd imports
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Dashboard, { dashboardAction, dashboardLoader } from "./pages/Dashboard";
+import Main, { mainLoader } from "./layouts/Main";
+import Error from "./pages/Error";
+import { logoutAction } from "./actions/logout";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ExpensesPage, {
+  expensesAction,
+  expensesLoader,
+} from "./pages/ExpensesPage";
+import BudgetPage, { budgetAction, budgetLoader } from "./pages/BudgetPage";
+import { deleteBudget } from "./actions/deleteBudget";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Hi</h1>,
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true, // same as path: "/",
+        element: <Dashboard />,
+        loader: dashboardLoader,
+        action: dashboardAction,
+        errorElement: <Error />,
+      },
+      {
+        path: "budget/:id",
+        element: <BudgetPage />,
+        loader: budgetLoader,
+        action: budgetAction,
+        errorElement: <Error />,
+        children: [
+          {
+            path: "delete",
+            action: deleteBudget,
+          },
+        ],
+      },
+      {
+        path: "expenses",
+        element: <ExpensesPage />,
+        loader: expensesLoader,
+        action: expensesAction,
+        errorElement: <Error />,
+      },
+      { path: "logout", action: logoutAction },
+    ],
   },
 ]);
 
@@ -11,6 +58,7 @@ function App() {
   return (
     <div className="App">
       <RouterProvider router={router} />
+      <ToastContainer />
     </div>
   );
 }
